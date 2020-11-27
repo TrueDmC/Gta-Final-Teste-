820a081a3967776603e15fa3258253b9c54dc3f0
Renan Borges Gouveia <rbgmarketplay@gmail.com>
2020-11-27 12:19:35 -0300

    Modificado Jogo alteraçoes

--- Assets/How_To_Do_Touch_system/How_to_doTouch_system/Touch Controls/Scripts/Controllers/SensitiveJoystick.cs
+++ Assets/How_To_Do_Touch_system/How_to_doTouch_system/Touch Controls/Scripts/Controllers/SensitiveJoystick.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
 
 namespace How_To_Do
 {
+    
     public class SensitiveJoystick : SimpleJoystick
     {
+        public CarController carromovimento;
+
         public AnimationCurve SensitivityCurve = new AnimationCurve(
             new Keyframe(0f, 0f, 1f, 1f),
             new Keyframe(1f, 1f, 1f, 1f));
@@ -12,15 +16,18 @@ namespace How_To_Do
         public override void OnDrag(PointerEventData eventData)
         {
             base.OnDrag(eventData);
-
+         
             var linearHorizontalValue = HorizintalAxis.Value;
             var linearVecticalValue = VerticalAxis.Value;
 
             var horizontalSign = Mathf.Sign(linearHorizontalValue);
             var verticalSign = Mathf.Sign(linearVecticalValue);
 
+
             HorizintalAxis.Value = horizontalSign * SensitivityCurve.Evaluate(horizontalSign * linearHorizontalValue);
             VerticalAxis.Value = verticalSign * SensitivityCurve.Evaluate(verticalSign * linearVecticalValue);
+
+            carromovimento.Move(HorizintalAxis.Value, VerticalAxis.Value, 0, 0);
         }
     }
 }
